00206b47fe6751b24cc843c066156428b5182929
Dmitry Bobrov <dmitry.bobrov@booking.com>
2018-01-15 14:15:14 +0100

    Builder image: build from different branches

--- images/001_replicator_builder/run.sh
+++ images/001_replicator_builder/run.sh
@@ -1,11 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+BRANCH="$1"
+set -eu -o pipefail
+if [ -z "$BRANCH" ]; then
+    BRANCH=master
+fi
+
 git clone https://github.com/mysql-time-machine/replicator.git
 
 cd /replicator
 
+git checkout "$BRANCH"
+
 mvn package
 
-cp /replicator/target/mysql-replicator-0.14.2.jar /output/mysql-replicator.jar
+eval $(printf 'VERSION=${project.version}\n0\n' | mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate | grep '^VERSION')
+
+cp "/replicator/target/mysql-replicator-$VERSION.jar" /output/mysql-replicator.jar
 
 # TODO:
 #   - run in the same pod as mysql
